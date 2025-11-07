09a49c71df35d8a875c19b4c775da0255ed8511f
Vishnu Murthy <158474952+murthy30300@users.noreply.github.com>
2025-11-07 21:54:31 +0530

    Fix API_BASE_URL in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  API_BASE_URL: "http://s2s-backend-host.us-east-1.elasticbeanstalk.com/", // change to EC2 URL when deploying
+  //API_BASE_URL: "http://localhost:1987",
+   API_BASE_URL: "http://s2s-backend-host.us-east-1.elasticbeanstalk.com", // change to EC2 URL when deploying
 };
 
 export default CONFIG;
